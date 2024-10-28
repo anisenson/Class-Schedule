f73b6a6704c939a2ee6b287f683feeb3032fd97d
anisenson <adamjn524@gmail.com>
2024-10-28 11:24:26 -0400

    Making Progress

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-$(function () {
-const btn = $("#submitDay")
 
-btn.on('click', function() {
-    $.ajax({
-        url: `https://api.npoint.io/b13a50d34ac534a5970e`,
-        method: 'GET',
+$(document).ready(function () {
+    const btn = $("#submitDay");
+
+    btn.on('click', function () {
+        const selectedDay = $("#dayInput");
+
+        
+        $.ajax({
+            url: `https://api.npoint.io/b13a50d34ac534a5970e`,
+            method: 'GET',
+            success: function (data) {
+                const schedule = data.schedule
+                console.log(schedule);
+    
+            },
+            error: function () {
+                alert("Theres a connection error");
+            }
+        });
     })
 })
-
-
-
-});
