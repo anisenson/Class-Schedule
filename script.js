2850520b0d6857c21fa94cb7c84edc7a197497a5
anisenson <adamjn524@gmail.com>
2024-10-28 11:35:51 -0400

    Added comments

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-
 $(document).ready(function () {
-    const btn = $("#submitDay");
+    const btn = $("#submitDay");    //set btn to the button
 
-    btn.on('click', function () {
-        const selectedDay = $("#dayInput");
+    btn.on('click', function () {   //on click, run function
+
+       
+        const selectedDay = $("#dayInput"); // the selected day
 
         
-        $.ajax({
-            url: `https://api.npoint.io/b13a50d34ac534a5970e`,
-            method: 'GET',
+        $.ajax({ 
+            url: `https://api.npoint.io/b13a50d34ac534a5970e`, // ajax file
+            method: 'GET', //gets the file
             success: function (data) {
                 const schedule = data.schedule
                 console.log(schedule);
     
             },
-            error: function () {
+            error: function () { //error alert if file doesnt work
                 alert("Theres a connection error");
             }
         });
+
+
+
+
+
+
+
     })
 })
