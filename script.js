5b788ae8539436eb7f28a893c5b0dc7942bc4d0f
anisenson <adamjn524@gmail.com>
2024-10-30 09:28:11 -0400

    Added Bell Schedule

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
     const btn = $("#submitDay");    //set btn to the button
 
+    const bellSchedule = {
+        1: { start: '8:24 AM', end: '9:31 AM' },
+        2: { start: '9:36 AM', end: '10:43 AM' },
+        3: { start: '10:48 AM', end: '11:55 AM' },
+        lunch: { start: '12:00 PM', end: '12:35 PM' },
+        4: { start: '12:41 PM', end: '1:48 PM' },
+        5: { start: '1:53 PM', end: '3:00 PM' }
+    };
+    
     btn.on('click', function () {   //on click, run function
 
        
